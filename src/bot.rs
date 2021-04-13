e782a29ce978e1b051299fc4325e0d7f6c899b7a
Mubelotix <mubelotix@gmail.com>
2021-04-13 15:44:12 +0200

    Change project structure

---
+++ src/bot.rs
@@ -0,0 +1,81 @@
+use std::sync::{Mutex, Arc};
+use std::time::Instant;
+use crate::network::connect;
+use minecraft_format::{
+    packets::{play_clientbound::ClientboundPacket, play_serverbound::ServerboundPacket},
+    MinecraftPacketPart,
+};
+
+pub struct Bot {}
+
+impl Bot {
+    pub fn create(addr: &str, port: u16, username: &str) {
+        let (receiver, sender) = connect(addr, port, username);
+        let bot = Arc::new(Mutex::new(Bot{}));
+        let bot2 = Arc::clone(&bot);
+        let sender2 = sender.clone();
+
+        std::thread::spawn(move || {
+            loop {
+                let mut packet = receiver.recv().unwrap();
+                let packet = match ClientboundPacket::deserialize_uncompressed_minecraft_packet(packet.as_mut_slice()) {
+                    Ok(packet) => packet,
+                    Err(e) => {
+                        eprintln!("Failed to parse clientbound packet: {}", e);
+                        continue;
+                    }
+                };
+                let mut bot = bot.lock().unwrap();
+                let response_packets = bot.update(packet);
+                for response_packet in response_packets {
+                    let response_packet = match response_packet.serialize_minecraft_packet() {
+                        Ok(response_packet) => response_packet,
+                        Err(e) => {
+                            println!("Failed to serialize packet from client {}", e);
+                            continue;
+                        },
+                    };
+                    sender.send(response_packet).unwrap();
+                }
+            }
+        });
+
+        std::thread::spawn(move || {
+            loop {
+                let start_time = Instant::now();
+                let mut bot = bot2.lock().unwrap();
+                let response_packets = bot.act();
+                for response_packet in response_packets {
+                    let response_packet = match response_packet.serialize_minecraft_packet() {
+                        Ok(response_packet) => response_packet,
+                        Err(e) => {
+                            println!("Failed to serialize packet from client {}", e);
+                            continue;
+                        },
+                    };
+                    sender2.send(response_packet).unwrap();
+                }
+                let elapsed_time = Instant::now() - start_time;
+                std::thread::sleep(std::time::Duration::from_millis(50) - elapsed_time);
+            }
+        });
+    }
+
+    pub fn act(&mut self) -> Vec<ServerboundPacket> {
+        Vec::new()
+    }
+
+    pub fn update(&mut self, packet: ClientboundPacket) -> Vec<ServerboundPacket> {
+        let mut responses = Vec::new();
+        match packet {
+            ClientboundPacket::KeepAlive { keep_alive_id } => {
+                responses.push(ServerboundPacket::KeepAlive{keep_alive_id});
+            },
+            ClientboundPacket::ChunkData { value: _ } => {
+                // TODO
+            },
+            _ => (),
+        }
+        responses
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,7 @@
-use minecraft_format::packets::{
-    play_clientbound::ClientboundPacket, play_serverbound::ServerboundPacket,
-};
-use minecraft_format::*;
-
 pub mod network;
-use network::*;
+pub mod bot;
+use bot::Bot;
 
 fn main() {
-    let (receiver, sender) = connect("127.0.0.1", 25565, "bot");
-
-    loop {
-        let mut packet_bytes = receiver.recv().unwrap();
-        let packet =
-            ClientboundPacket::deserialize_uncompressed_minecraft_packet(&mut packet_bytes);
-        let packet = match packet {
-            Ok(packet) => packet,
-            Err(e) => panic!("{} for {:?}", e, packet_bytes),
-        };
-        match packet {
-            ClientboundPacket::KeepAlive { keep_alive_id } => {
-                sender.send(
-                    ServerboundPacket::KeepAlive { keep_alive_id }.serialize_minecraft_packet().unwrap(),
-                )
-                .unwrap();
-                println!("pong!");
-            }
-            ClientboundPacket::ChunkData { mut value } => {
-                value.deserialize_chunk_sections().unwrap();
-                println!("chunk parsed successfully!")
-            }
-            ClientboundPacket::ChatMessage {
-                message,
-                position: _,
-                sender,
-            } => {
-                println!("{}: {}", sender, message);
-            }
-            _ => (),
-        }
-    }
+    Bot::create("127.0.0.1", 25565, "bot");
 }
