8aee80abe571a42f7c44d382c7e4e3a55654f0b1
Mubelotix <mubelotix@gmail.com>
2021-04-24 16:29:40 +0200

    Implement TravelMission better

--- src/missions/travel.rs
+++ src/missions/travel.rs
@@ -1,18 +1,17 @@
+use crate::*;
 use minecraft_format::packets::play_serverbound::ServerboundPacket;
-
-use crate::{bot::Bot, map::Map, pathfinder::Path};
-
+use std::cmp::Ordering;
 use super::MissionResult;
 
 #[derive(Debug)]
 pub struct TravelMission {
-    path: Path,
+    path: Vec<(i32, i32, i32)>,
 }
 
 impl TravelMission {
     pub fn new(map: &Map, position: (i32, i32, i32), destination: (i32, i32, i32)) -> Option<Self> {
         Some(Self {
-            path: Path::find_path(map, position, destination)?
+            path: find_path(map, position, destination)?,
         })
     }
 }
@@ -20,12 +19,65 @@ impl TravelMission {
 impl super::Mission for TravelMission {
     fn execute(&mut self, bot: &mut Bot, _packets: &mut Vec<ServerboundPacket>) -> MissionResult {
         if let Some(position) = bot.position.as_mut() {
-            if let Some(((x, z), jump)) = self.path.follow((position.x, position.y, position.z), &bot.map) {
-                position.x = x;
-                position.z = z;
-                if jump {
-                    bot.vertical_speed = 0.4;
+            let ((x, z), jump) = {
+                let mut position = (position.x, position.y, position.z);
+                let x = position.0.floor() as i32;
+                let y = position.1.floor() as i32;
+                let z = position.2.floor() as i32;
+                let mut jump = false;
+
+                let next_position = match self.path.get(0) {
+                    Some(next) => next,
+                    None => return MissionResult::Done,
+                };
+                if next_position.0 == x && next_position.2 == z && (y - 2..=y).contains(&next_position.1) {
+                    self.path.remove(0);
+                    return MissionResult::InProgress;
                 }
+
+                if next_position.1 > y && bot.map.is_on_ground(position.0, position.1, position.2) {
+                    jump = true;
+                }
+                let mut movement_required = (next_position.0 as f64 + 0.5 - position.0).abs();
+                if movement_required > 0.2 {
+                    movement_required = 0.2;
+                }
+
+                match (next_position.0 as f64 + 0.5).partial_cmp(&position.0) {
+                    Some(Ordering::Less) => {
+                        let max = bot.map.max_west_movement(position.0, position.1, position.2);
+                        position.0 -= if max > movement_required { movement_required } else { max };
+                    }
+                    Some(Ordering::Greater) => {
+                        let max = bot.map.max_east_movement(position.0, position.1, position.2);
+                        position.0 += if max > movement_required { movement_required } else { max };
+                    }
+                    _ => {}
+                }
+
+                let mut movement_required = (next_position.2 as f64 + 0.5 - position.2).abs();
+                if movement_required > 0.2 {
+                    movement_required = 0.2;
+                }
+                match (next_position.2 as f64 + 0.5).partial_cmp(&position.2) {
+                    Some(Ordering::Less) => {
+                        let max = bot.map.max_north_movement(position.0, position.1, position.2);
+                        position.2 -= if max > movement_required { movement_required } else { max };
+                    }
+                    Some(Ordering::Greater) => {
+                        let max = bot.map.max_south_movement(position.0, position.1, position.2);
+                        position.2 += if max > movement_required { movement_required } else { max };
+                    }
+                    _ => {}
+                }
+
+                ((position.0, position.2), jump)
+            };
+
+            position.x = x;
+            position.z = z;
+            if jump {
+                bot.vertical_speed = 0.4;
             }
         }
         MissionResult::InProgress
--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -1,18 +1,8 @@
 use crate::map::Map;
 use log::*;
-use minecraft_format::packets::Direction;
-use std::{cell::RefCell, collections::{BTreeMap, BinaryHeap}};
+use std::collections::{BinaryHeap};
 
-// todo listen for block change and update path as the map is modified
 // todo consider some blocks as liquid and some as transparent
-#[derive(Debug)]
-pub struct Path {
-    path: Vec<(i32, i32, i32)>,
-    can_log: bool,
-}
-
-type Accesses = Vec<((i32, i32, i32), usize)>;
-type AccessibleBlock = BTreeMap<(i32, i32, i32), ((i32, i32, i32), usize)>;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Node {
@@ -36,7 +26,7 @@ impl Node {
             x: position.0,
             y: position.1,
             z: position.2,
-            origin
+            origin,
         }
     }
 
@@ -60,14 +50,11 @@ impl Node {
 
     #[allow(clippy::short_circuit_statement)]
     #[allow(unused_must_use)]
-    pub fn close(
-        self,
-        map: &Map,
-        destination: (i32, i32, i32),
-        closed_nodes: &mut Vec<Node>,
-        open_nodes: &mut BinaryHeap<Node>,
-    ) {
-        if closed_nodes.iter().any(|e| e.x == self.x && e.z == self.z && e.y == self.y && e.g_cost <= self.g_cost) {
+    pub fn close(self, map: &Map, destination: (i32, i32, i32), closed_nodes: &mut Vec<Node>, open_nodes: &mut BinaryHeap<Node>) {
+        if closed_nodes
+            .iter()
+            .any(|e| e.x == self.x && e.z == self.z && e.y == self.y && e.g_cost <= self.g_cost)
+        {
             return;
         }
 
@@ -95,7 +82,12 @@ impl Node {
                     }
                 }
                 if map.get_block(x, self.y - 1 - offset, z).is_blocking() {
-                    open_nodes.push(Node::new((x, self.y - offset, z), (self.x, self.y, self.z), destination, self.g_cost + 10));
+                    open_nodes.push(Node::new(
+                        (x, self.y - offset, z),
+                        (self.x, self.y, self.z),
+                        destination,
+                        self.g_cost + 10,
+                    ));
                     return true;
                 }
             }
@@ -124,130 +116,50 @@ impl std::cmp::Ord for Node {
     }
 }
 
-impl Path {
-    pub fn find_path(map: &Map, position: (i32, i32, i32), destination: (i32, i32, i32)) -> Option<Path> {
-        let mut closed_nodes: Vec<Node> = Vec::new();
-        let mut open_nodes: BinaryHeap<Node> = BinaryHeap::new();
-        open_nodes.push(Node::new(position, destination, position, 0));
+pub fn find_path(map: &Map, position: (i32, i32, i32), destination: (i32, i32, i32)) -> Option<Vec<(i32, i32, i32)>> {
+    let mut closed_nodes: Vec<Node> = Vec::new();
+    let mut open_nodes: BinaryHeap<Node> = BinaryHeap::new();
+    open_nodes.push(Node::new(position, destination, position, 0));
 
-        let mut counter = 0;
-        let start_instant = std::time::Instant::now();
-        while let Some(node) = open_nodes.pop() {
-            if node.x == destination.0 && node.y == destination.1 && node.z == destination.2 {
-                trace!("Found destination");
-                trace!("Finished the loop in {}ms. {} closed nodes", start_instant.elapsed().as_millis(), closed_nodes.len());
+    let mut counter = 0;
+    let start_instant = std::time::Instant::now();
+    while let Some(node) = open_nodes.pop() {
+        if node.x == destination.0 && node.y == destination.1 && node.z == destination.2 {
+            trace!("Found destination");
+            trace!(
+                "Finished the loop in {}ms. {} closed nodes",
+                start_instant.elapsed().as_millis(),
+                closed_nodes.len()
+            );
 
-                let mut path = Vec::new();
-                let mut node = node;
-                loop {
-                    path.push((node.x, node.y, node.z));
-                    let next_node_idx = match closed_nodes.iter().position(|n| n.x == node.origin.0 && n.y == node.origin.1 && n.z == node.origin.2) {
-                        Some(idx) => idx,
-                        None => break,
-                    };
-                    node = closed_nodes.remove(next_node_idx);
-                }
-                path.reverse();
-                trace!("Path len: {}", path.len());
-        
-                return Some(Path { path, can_log: true, })
+            let mut path = Vec::new();
+            let mut node = node;
+            loop {
+                path.push((node.x, node.y, node.z));
+                let next_node_idx = match closed_nodes
+                    .iter()
+                    .position(|n| n.x == node.origin.0 && n.y == node.origin.1 && n.z == node.origin.2)
+                {
+                    Some(idx) => idx,
+                    None => break,
+                };
+                node = closed_nodes.remove(next_node_idx);
             }
+            path.reverse();
+            trace!("Path len: {}", path.len());
 
-            node.close(&map, destination, &mut closed_nodes, &mut open_nodes);
-
-            counter += 1;
-            if counter > 7500 {
-                warn!("Could not find the destination in time. {}ms used", start_instant.elapsed().as_millis());
-                return None;
-            }
+            return Some(path);
         }
 
-        trace!("Unreachable destination");
-        None
+        node.close(&map, destination, &mut closed_nodes, &mut open_nodes);
+
+        counter += 1;
+        if counter > 7500 {
+            warn!("Could not find the destination in time. {}ms used", start_instant.elapsed().as_millis());
+            return None;
+        }
     }
 
-    pub fn follow(&mut self, mut position: (f64, f64, f64), map: &Map) -> Option<((f64, f64), bool)> {
-        let x = position.0.floor() as i32;
-        let y = position.1.floor() as i32;
-        let z = position.2.floor() as i32;
-        let mut jump = false;
-
-        let mut current_idx = None;
-        for (idx, position) in self.path.iter().enumerate() {
-            if position.0 == x && position.2 == z && (y-2..=y).contains(&position.1) {
-                current_idx = Some(idx);
-                break
-            }
-        }
-        let current_idx = match current_idx {
-            Some(current_idx) => current_idx,
-            None => {
-                if !self.can_log {
-                    warn!("Out of path. Current position {} {} {} not found", x, y, z);
-                    self.can_log = true;
-                }
-                return None;
-            }
-        };
-
-        if self.path.len() > 1 && current_idx > 0 {
-            for _ in 0..current_idx {
-                self.path.remove(0);
-            }
-        }
-
-        let next_position = match self.path.get(1) {
-            Some(next_position) => {
-                *next_position
-            },
-            None => {
-                if !self.can_log {
-                    warn!("Destination achieved! No path fragment after idx {}", current_idx + 1);
-                    self.can_log = true;
-                }
-                return None;
-            }
-        };
-        self.can_log = false;
-
-        if next_position.1 > y && map.is_on_ground(position.0, position.1, position.2) {
-            jump = true;
-        }
-        let mut movement_required = (next_position.0 as f64 + 0.5 - position.0).abs();
-        if movement_required > 0.2 {
-            movement_required = 0.2;
-        }
-        //trace!("{} x is {} but targets {}", current_idx, position.0, next_position.0 as f64 + 0.5);
-        match (next_position.0 as f64 + 0.5).partial_cmp(&position.0) {
-            Some(std::cmp::Ordering::Less) => {
-                let max = map.max_west_movement(position.0, position.1, position.2);
-                position.0 -= if max > movement_required { movement_required } else {max};
-            }
-            Some(std::cmp::Ordering::Greater) => {
-                let max = map.max_east_movement(position.0, position.1, position.2);
-                position.0 += if max > movement_required { movement_required } else {max};
-            }
-            _ => {}
-        }
-
-        let mut movement_required = (next_position.2 as f64 + 0.5 - position.2).abs();
-        if movement_required > 0.2 {
-            movement_required = 0.2;
-        }
-        match (next_position.2 as f64 + 0.5).partial_cmp(&position.2) {
-            Some(std::cmp::Ordering::Less) => {
-                let max = map.max_north_movement(position.0, position.1, position.2);
-                position.2 -= if max > movement_required { movement_required } else {max};
-            }
-            Some(std::cmp::Ordering::Greater) => {
-                let max = map.max_south_movement(position.0, position.1, position.2);
-                position.2 += if max > movement_required { movement_required } else {max};
-            }
-            _ => {}
-        }
-        //trace!("z is {} but targets {}", position.2, next_position.2 as f64 + 0.5);
-        
-
-        Some(((position.0, position.2), jump))
-    }
+    trace!("Unreachable destination");
+    None
 }
